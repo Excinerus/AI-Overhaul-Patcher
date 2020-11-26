e700d29d9c2867057d68356f73937ed0dd6c0426
Ahmed Jlassi <excinerus@gmail.com>
2020-11-26 12:19:45 +0100

    v1

---
+++ AIOverhaulPatcher/Program.cs
@@ -0,0 +1,138 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Mutagen.Bethesda;
+using Mutagen.Bethesda.Synthesis;
+using Mutagen.Bethesda.Skyrim;
+using AIOverhaulPatcher.Utilities;
+namespace AIOverhaulPatcher
+{
+    public class Program
+    {
+
+        public static int Main(string[] args)
+        {
+            return SynthesisPipeline.Instance.Patch<ISkyrimMod, ISkyrimModGetter>(
+                args: args,
+                patcher: RunPatch,
+                userPreferences: new UserPreferences()
+                {
+                    ActionsForEmptyArgs = new RunDefaultPatcher()
+                    {
+                        IdentifyingModKey = "AIOPatch.esp",
+                        TargetRelease = GameRelease.SkyrimSE,
+                        BlockAutomaticExit = true,
+                    }
+                });
+        }
+
+        public static void RunPatch(SynthesisState<ISkyrimMod, ISkyrimModGetter> state)
+        {
+
+            var AIOverhaul = state.LoadOrder.GetModByFileName( "AI Overhaul.esp");
+            var USLEEP = state.LoadOrder.GetModByFileName("Unofficial Skyrim Special Edition Patch.esp");
+            if (USLEEP!=null ) System.Console.WriteLine("Unofficial Skyrim Special Edition Patch.esp");
+            var UsleepOrder = state.LoadOrder.GetFileOrder("Unofficial Skyrim Special Edition Patch.esp");
+            System.Console.WriteLine("at " + UsleepOrder);
+            //Your code here!
+
+            if (AIOverhaul == null)
+            {
+                
+                System.Console.WriteLine("AIOverhaul.esp not found");
+                return;
+            }
+
+
+            int bmax = 10;
+            int b = 0;
+            int processed = 0;
+            int total = AIOverhaul.Npcs.Count;
+
+            var AIOFormIDs = AIOverhaul.Npcs.Select(x => x.FormKey.ID).ToList();
+            var winningOverrides = state.LoadOrder.PriorityOrder.WinningOverrides<INpcGetter>().Where(x => AIOFormIDs.Contains(x.FormKey.ID)).ToList();
+            var USLEEPandPrior = state.LoadOrder.PriorityOrder.Reverse().Take(UsleepOrder + 1).Select(x => x.Mod).ToList();
+            var masterfilenames = AIOverhaul.MasterReferences.Select(x => x.Master.FileName).ToList();
+            var MasterFiles = state.LoadOrder.PriorityOrder.Reverse().Where(x => masterfilenames.Contains(x.ModKey.FileName)).ToList();
+            var NPCMasters = MasterFiles.SelectMany(x => x.Mod?.GetTopLevelGroupGetter<INpcGetter>()).Select(x=>x.Value).Where(x => AIOFormIDs.Contains(x.FormKey.ID)).ToList();
+
+            var allOverrides = state.LoadOrder.PriorityOrder.Reverse().Skip(UsleepOrder + 1).Select(x => x.Mod).SelectMany(x => x?.GetTopLevelGroupGetter<INpcGetter>()).Where(x => AIOFormIDs.Contains(x.Value.FormKey.ID)).Select(x => x.Value).ToList();
+         
+
+        System.Console.WriteLine(processed + "/" + total + " Npcs");
+            foreach (var npc in AIOverhaul.Npcs)
+            {
+
+                if (b >= bmax)
+                {
+                    b = 0;
+                    System.Console.WriteLine(processed + "/" + total + " Npcs");
+                }
+
+                var winningOverride = winningOverrides.Where(x => x.FormKey.ID == npc.FormKey.ID).FirstOrDefault();
+                var Masters = NPCMasters.Where(x => x.FormKey.ID == npc.FormKey.ID).ToList();
+                var winningMaster = Masters.FirstOrDefault();
+                if (winningMaster == null) winningMaster = state.LoadOrder.PriorityOrder.Select(x => x.Mod).SelectMany(x => x?.GetTopLevelGroupGetter<INpcGetter>()).Where(x => x.Value.FormKey.ID == npc.FormKey.ID).Select(x => x.Value).First();
+                var overrides = allOverrides.Where(x => x.FormKey.ID == npc.FormKey.ID).ToList();
+
+                var patchNpc = state.PatchMod.Npcs.GetOrAddAsOverride(winningOverride);
+                if (winningOverride != npc)
+                {
+                    if (npc.IsProtected()) patchNpc.Configuration.Flags.SetProtected(true, true);
+
+                    foreach (var fac in npc.Factions)
+                        if (!patchNpc.Factions.Select(x => new KeyValuePair<uint,int> (x.Faction.FormKey.ID , x.Rank)).Contains(new KeyValuePair<uint, int>(fac.Faction.FormKey.ID, fac.Rank)))
+                        {
+                            patchNpc.Factions.Add(  fac.DeepCopy());
+                        }
+
+                     
+                    var PackagesToRemove = Masters.SelectMany(x=>x.Packages).Select(x=>x.FormKey.ID).Where(x => !npc.Packages.Select(x=>x.FormKey.ID).Contains(x)).ToList();
+
+                    List<FormKey> PackagesToAdd = overrides.SelectMany(x => x.Packages).Select(x => x.FormKey).Where(x => !PackagesToRemove.Contains(x.ID)).Distinct().ToList();
+
+                    patchNpc.Packages.Clear();
+                    PackagesToAdd.ForEach(x => patchNpc.Packages.Add(x));
+
+
+                    var OverwrittenOutfits = Masters.Select(x => x.DefaultOutfit).Select(x => x.FormKey.HasValue ? x.FormKey.Value.ID : 0).Distinct().ToList();
+                    var OverwrittenSleepingOutfit = Masters.Select(x => x.SleepingOutfit).Select(x => x.FormKey.HasValue ? x.FormKey.Value.ID : 0).Distinct().ToList();
+
+                    FormKey? OverwrittingOutfit = overrides.Select(x => x.DefaultOutfit).Select(x => x.FormKey).Where(x => x != null && !OverwrittenOutfits.Contains(x.Value.ID)).Prepend(npc.DefaultOutfit.FormKey).Last();
+                    FormKey? OverwrittingSleepingOutfit = overrides.Select(x => x.SleepingOutfit).Select(x => x.FormKey).Where(x => x != null && !OverwrittenSleepingOutfit.Contains(x.Value.ID)).Prepend(npc.SleepingOutfit.FormKey).Last();
+
+                    patchNpc.DefaultOutfit = OverwrittingOutfit;
+                    patchNpc.SleepingOutfit = OverwrittingSleepingOutfit;
+
+                    patchNpc.SpectatorOverridePackageList = npc.SpectatorOverridePackageList.FormKey;
+                    patchNpc.CombatOverridePackageList = npc.CombatOverridePackageList.FormKey;
+                    patchNpc.AIData.Confidence = (Confidence)Math.Min((int)patchNpc.AIData.Confidence, (int)npc.AIData.Confidence);
+
+
+                    
+                    if (npc.VirtualMachineAdapter != null)
+                    {
+                        List<IScriptEntryGetter> ScriptsToForward = npc.VirtualMachineAdapter.Scripts.Where(x => patchNpc.VirtualMachineAdapter == null || !patchNpc.VirtualMachineAdapter.Scripts.Select(x => x.Name).Contains(x.Name)).ToList();
+                        if (ScriptsToForward.Count > 0)
+                        {
+                            if (patchNpc.VirtualMachineAdapter == null)
+                                patchNpc.VirtualMachineAdapter = npc.VirtualMachineAdapter.DeepCopy();
+                            else
+                            {
+                                ScriptsToForward.ForEach(x => patchNpc.VirtualMachineAdapter.Scripts.Add(x.DeepCopy()));
+                            }
+                        }
+
+                    }
+
+
+                }
+                b++;
+                processed++;
+            }
+
+
+
+        }
+    }
+}
---
+++ AIOverhaulPatcher/Utilities/ExLoadOrderUtilities.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Mutagen.Bethesda;
+using Mutagen.Bethesda.Synthesis;
+using Mutagen.Bethesda.Skyrim;
+using System.Text;
+
+namespace AIOverhaulPatcher.Utilities
+{
+    public static class ExLoadOrderUtilities
+    {
+        public static ISkyrimModGetter? GetModByFileName( this LoadOrder<IModListing<ISkyrimModGetter>>? LoadOrder, string Name)
+        {
+            if (LoadOrder == null) return null;
+            var Mods = LoadOrder.Keys.Where(x => x.FileName.ToLower() == Name.ToLower()).ToList();
+            return (Mods.Count > 0) ?LoadOrder[Mods.First()].Mod : null;
+        }
+        public static TMajor? GetWinningOverride<TMajor> (this TMajor record , SynthesisState<ISkyrimMod, ISkyrimModGetter> state) where TMajor : class, IMajorRecordCommonGetter
+        {
+            if (record == null || state == null) return null;
+            var result = state.LoadOrder.PriorityOrder.WinningOverrides<TMajor>().Where(x => x.FormKey.ID == record.FormKey.ID).ToList();
+            return (result.Count > 0) ? result.First() : null;
+        }
+
+        public static TMajor? GetWinningOverrideAtOrder<TMajor>(this TMajor record, int Order, SynthesisState<ISkyrimMod, ISkyrimModGetter> state) where TMajor : class, IMajorRecordCommonGetter
+        {
+            if (record == null || state == null) return null;
+            var result = state.LoadOrder.PriorityOrder.Take(Order + 1).Select(x => x.Mod).Select(x=>x?.GetTopLevelGroupGetter<TMajor>()).SelectMany(x=>x?.Items).Where(x => (long) x.FormKey.ID == (long) record.FormKey.ID).ToList();
+            return (result.Count > 0) ? result.Last() : null;
+        }
+
+
+
+
+
+        public static int GetFileOrder (this LoadOrder<IModListing<ISkyrimModGetter>>? LoadOrder, string Filename)
+        {
+            if (Filename == null || LoadOrder == null) return -1;
+            return LoadOrder.Keys.ToList().FindIndex(0, LoadOrder.Keys.Count(), x => x.FileName.ToLower() == Filename.ToLower());
+        }
+ 
+        public static int GetOrder<TMajor>(this TMajor record, SynthesisState<ISkyrimMod, ISkyrimModGetter> state) where TMajor : class, IMajorRecordCommonGetter
+        {
+            if (record == null || state == null) return -1;
+            return state.LoadOrder.GetFileOrder(record.FormKey.ModKey.FileName  );
+        }
+        public static List<TMajor> GetMasterAndOverrides<TMajor>(this TMajor record, SynthesisState<ISkyrimMod, ISkyrimModGetter> state) where TMajor : class, IMajorRecordCommonGetter
+        {
+            return state.LoadOrder.SelectMany(x => x.Value.Mod?.GetTopLevelGroupGetter<TMajor>()).Where(x => x.Value.FormKey.ID == record.FormKey.ID).Select(x => x.Value).ToList();
+        }
+        public static List<TMajor> GetOverrides<TMajor>(this TMajor record, SynthesisState<ISkyrimMod, ISkyrimModGetter> state) where TMajor : class, IMajorRecordCommonGetter
+        {
+            var order = record.GetOrder(state);
+            return state.LoadOrder.PriorityOrder.Skip(order+1).Select(x => x.Mod).SelectMany(x=>x?.GetTopLevelGroupGetter<TMajor>()).Where(x => x.Value.FormKey.ID == record.FormKey.ID).Select(x => x.Value).ToList();
+        }
+        public static List<TMajor> GetOverriden<TMajor>(this TMajor record, SynthesisState<ISkyrimMod, ISkyrimModGetter> state) where TMajor : class, IMajorRecordCommonGetter
+        {
+            var order = record.GetOrder(state);
+            return state.LoadOrder.PriorityOrder.Take(order).Select(x => x.Mod).SelectMany(x => x?.GetTopLevelGroupGetter<TMajor>()).Where(x => x.Value.FormKey.ID == record.FormKey.ID).Select(x => x.Value).ToList();
+        }
+        public static TMajor GetMaster<TMajor>(this TMajor record, SynthesisState<ISkyrimMod, ISkyrimModGetter> state) where TMajor : class, IMajorRecordCommonGetter
+        {
+            return state.LoadOrder.PriorityOrder.Select(x => x.Mod).SelectMany(x=>x?.GetTopLevelGroupGetter<TMajor>()).Where(x => x.Value.FormKey.ID == record.FormKey.ID).Select(x => x.Value).First();
+        }
+        public static TMajor? GetFromFile<TMajor>(this TMajor record, string FileName, SynthesisState<ISkyrimMod, ISkyrimModGetter> state) where TMajor : class, IMajorRecordCommonGetter
+        {
+            return state?.LoadOrder?.GetModByFileName(FileName)?.GetTopLevelGroupGetter<TMajor>().Where(x => x.Value.FormKey.ID == record.FormKey.ID).FirstOrDefault()?.Value;
+        }
+        public static TMajor? GetFromMod<TMajor>(this TMajor record, ISkyrimModGetter? Mod) where TMajor : class, IMajorRecordCommonGetter
+        {
+            return Mod?.GetTopLevelGroupGetter<TMajor>().Where(x => x.Value.FormKey.ID == record.FormKey.ID).FirstOrDefault()?.Value;
+        }
+
+
+        public static void SetEssential(this NpcConfiguration.Flag Flags , bool value) => Flags = value==Flags.HasFlag(NpcConfiguration.Flag.Essential)?Flags: value ? (Flags & (~NpcConfiguration.Flag.Protected)) | NpcConfiguration.Flag.Essential : (Flags & (~NpcConfiguration.Flag.Essential));
+        public static void SetProtected(this NpcConfiguration.Flag Flags, bool value, bool ignoreIfEssential = true) => Flags =  value == Flags.HasFlag(NpcConfiguration.Flag.Protected) ||(Flags.HasFlag(NpcConfiguration.Flag.Essential) && ignoreIfEssential)? Flags : value ? (Flags & (~NpcConfiguration.Flag.Essential)) | NpcConfiguration.Flag.Essential : (Flags & (~NpcConfiguration.Flag.Protected));
+
+        public static bool IsProtected(this INpcGetter npc) => npc.Configuration.Flags.HasFlag(NpcConfiguration.Flag.Protected);
+        public static bool IsEssential(this INpcGetter npc) => npc.Configuration.Flags.HasFlag(NpcConfiguration.Flag.Essential);
+
+
+
+
+    }
+}
