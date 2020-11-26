33da88d0e501827f91a99262369ba6dfdb7dc6ca
Ahmed Jlassi <excinerus@gmail.com>
2020-11-26 12:27:49 +0100

    cleaning extensions

--- AIOverhaulPatcher/Utilities/ExLoadOrderUtilities.cs
+++ AIOverhaulPatcher/Utilities/ExLoadOrderUtilities.cs
@@ -16,19 +16,19 @@ namespace AIOverhaulPatcher.Utilities
             var Mods = LoadOrder.Keys.Where(x => x.FileName.ToLower() == Name.ToLower()).ToList();
             return (Mods.Count > 0) ?LoadOrder[Mods.First()].Mod : null;
         }
-        public static TMajor? GetWinningOverride<TMajor> (this TMajor record , SynthesisState<ISkyrimMod, ISkyrimModGetter> state) where TMajor : class, IMajorRecordCommonGetter
-        {
-            if (record == null || state == null) return null;
-            var result = state.LoadOrder.PriorityOrder.WinningOverrides<TMajor>().Where(x => x.FormKey.ID == record.FormKey.ID).ToList();
-            return (result.Count > 0) ? result.First() : null;
-        }
+        //public static TMajor? GetWinningOverride<TMajor> (this TMajor record , SynthesisState<ISkyrimMod, ISkyrimModGetter> state) where TMajor : class, IMajorRecordCommonGetter
+        //{
+        //    if (record == null || state == null) return null;
+        //    var result = state.LoadOrder.PriorityOrder.WinningOverrides<TMajor>().Where(x => x.FormKey.ID == record.FormKey.ID).ToList();
+        //    return (result.Count > 0) ? result.First() : null;
+        //}
 
-        public static TMajor? GetWinningOverrideAtOrder<TMajor>(this TMajor record, int Order, SynthesisState<ISkyrimMod, ISkyrimModGetter> state) where TMajor : class, IMajorRecordCommonGetter
-        {
-            if (record == null || state == null) return null;
-            var result = state.LoadOrder.PriorityOrder.Take(Order + 1).Select(x => x.Mod).Select(x=>x?.GetTopLevelGroupGetter<TMajor>()).SelectMany(x=>x?.Items).Where(x => (long) x.FormKey.ID == (long) record.FormKey.ID).ToList();
-            return (result.Count > 0) ? result.Last() : null;
-        }
+        //public static TMajor? GetWinningOverrideAtOrder<TMajor>(this TMajor record, int Order, SynthesisState<ISkyrimMod, ISkyrimModGetter> state) where TMajor : class, IMajorRecordCommonGetter
+        //{
+        //    if (record == null || state == null) return null;
+        //    var result = state.LoadOrder.PriorityOrder.Take(Order + 1).Select(x => x.Mod).Select(x=>x?.GetTopLevelGroupGetter<TMajor>()).SelectMany(x=>x?.Items).Where(x => (long) x.FormKey.ID == (long) record.FormKey.ID).ToList();
+        //    return (result.Count > 0) ? result.Last() : null;
+        //}
 
 
 
@@ -45,32 +45,33 @@ namespace AIOverhaulPatcher.Utilities
             if (record == null || state == null) return -1;
             return state.LoadOrder.GetFileOrder(record.FormKey.ModKey.FileName  );
         }
-        public static List<TMajor> GetMasterAndOverrides<TMajor>(this TMajor record, SynthesisState<ISkyrimMod, ISkyrimModGetter> state) where TMajor : class, IMajorRecordCommonGetter
-        {
-            return state.LoadOrder.SelectMany(x => x.Value.Mod?.GetTopLevelGroupGetter<TMajor>()).Where(x => x.Value.FormKey.ID == record.FormKey.ID).Select(x => x.Value).ToList();
-        }
-        public static List<TMajor> GetOverrides<TMajor>(this TMajor record, SynthesisState<ISkyrimMod, ISkyrimModGetter> state) where TMajor : class, IMajorRecordCommonGetter
-        {
-            var order = record.GetOrder(state);
-            return state.LoadOrder.PriorityOrder.Skip(order+1).Select(x => x.Mod).SelectMany(x=>x?.GetTopLevelGroupGetter<TMajor>()).Where(x => x.Value.FormKey.ID == record.FormKey.ID).Select(x => x.Value).ToList();
-        }
-        public static List<TMajor> GetOverriden<TMajor>(this TMajor record, SynthesisState<ISkyrimMod, ISkyrimModGetter> state) where TMajor : class, IMajorRecordCommonGetter
-        {
-            var order = record.GetOrder(state);
-            return state.LoadOrder.PriorityOrder.Take(order).Select(x => x.Mod).SelectMany(x => x?.GetTopLevelGroupGetter<TMajor>()).Where(x => x.Value.FormKey.ID == record.FormKey.ID).Select(x => x.Value).ToList();
-        }
-        public static TMajor GetMaster<TMajor>(this TMajor record, SynthesisState<ISkyrimMod, ISkyrimModGetter> state) where TMajor : class, IMajorRecordCommonGetter
-        {
-            return state.LoadOrder.PriorityOrder.Select(x => x.Mod).SelectMany(x=>x?.GetTopLevelGroupGetter<TMajor>()).Where(x => x.Value.FormKey.ID == record.FormKey.ID).Select(x => x.Value).First();
-        }
-        public static TMajor? GetFromFile<TMajor>(this TMajor record, string FileName, SynthesisState<ISkyrimMod, ISkyrimModGetter> state) where TMajor : class, IMajorRecordCommonGetter
-        {
-            return state?.LoadOrder?.GetModByFileName(FileName)?.GetTopLevelGroupGetter<TMajor>().Where(x => x.Value.FormKey.ID == record.FormKey.ID).FirstOrDefault()?.Value;
-        }
-        public static TMajor? GetFromMod<TMajor>(this TMajor record, ISkyrimModGetter? Mod) where TMajor : class, IMajorRecordCommonGetter
-        {
-            return Mod?.GetTopLevelGroupGetter<TMajor>().Where(x => x.Value.FormKey.ID == record.FormKey.ID).FirstOrDefault()?.Value;
-        }
+
+        //public static List<TMajor> GetMasterAndOverrides<TMajor>(this TMajor record, SynthesisState<ISkyrimMod, ISkyrimModGetter> state) where TMajor : class, IMajorRecordCommonGetter
+        //{
+        //    return state.LoadOrder.SelectMany(x => x.Value.Mod?.GetTopLevelGroupGetter<TMajor>()).Where(x => x.Value.FormKey.ID == record.FormKey.ID).Select(x => x.Value).ToList();
+        //}
+        //public static List<TMajor> GetOverrides<TMajor>(this TMajor record, SynthesisState<ISkyrimMod, ISkyrimModGetter> state) where TMajor : class, IMajorRecordCommonGetter
+        //{
+        //    var order = record.GetOrder(state);
+        //    return state.LoadOrder.PriorityOrder.Reverse().Skip(order+1).Select(x => x.Mod).SelectMany(x=>x?.GetTopLevelGroupGetter<TMajor>()).Where(x => x.Value.FormKey.ID == record.FormKey.ID).Select(x => x.Value).ToList();
+        //}
+        //public static List<TMajor> GetOverriden<TMajor>(this TMajor record, SynthesisState<ISkyrimMod, ISkyrimModGetter> state) where TMajor : class, IMajorRecordCommonGetter
+        //{
+        //    var order = record.GetOrder(state);
+        //    return state.LoadOrder.PriorityOrder.Reverse().Take(order).Select(x => x.Mod).SelectMany(x => x?.GetTopLevelGroupGetter<TMajor>()).Where(x => x.Value.FormKey.ID == record.FormKey.ID).Select(x => x.Value).ToList();
+        //}
+        //public static TMajor GetMaster<TMajor>(this TMajor record, SynthesisState<ISkyrimMod, ISkyrimModGetter> state) where TMajor : class, IMajorRecordCommonGetter
+        //{
+        //    return state.LoadOrder.PriorityOrder.Reverse().Select(x => x.Mod).SelectMany(x=>x?.GetTopLevelGroupGetter<TMajor>()).Where(x => x.Value.FormKey.ID == record.FormKey.ID).Select(x => x.Value).First();
+        //}
+        //public static TMajor? GetFromFile<TMajor>(this TMajor record, string FileName, SynthesisState<ISkyrimMod, ISkyrimModGetter> state) where TMajor : class, IMajorRecordCommonGetter
+        //{
+        //    return state?.LoadOrder?.GetModByFileName(FileName)?.GetTopLevelGroupGetter<TMajor>().Where(x => x.Value.FormKey.ID == record.FormKey.ID).FirstOrDefault()?.Value;
+        //}
+        //public static TMajor? GetFromMod<TMajor>(this TMajor record, ISkyrimModGetter? Mod) where TMajor : class, IMajorRecordCommonGetter
+        //{
+        //    return Mod?.GetTopLevelGroupGetter<TMajor>().Where(x => x.Value.FormKey.ID == record.FormKey.ID).FirstOrDefault()?.Value;
+        //}
 
 
         public static void SetEssential(this NpcConfiguration.Flag Flags , bool value) => Flags = value==Flags.HasFlag(NpcConfiguration.Flag.Essential)?Flags: value ? (Flags & (~NpcConfiguration.Flag.Protected)) | NpcConfiguration.Flag.Essential : (Flags & (~NpcConfiguration.Flag.Essential));
