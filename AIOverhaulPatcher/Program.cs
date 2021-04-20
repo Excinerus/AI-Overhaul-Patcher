d692be98c8ccbf97755a7c26aed7389d989edac0
derickso <derickso@uci.edu>
2021-04-19 21:13:54 -0700

    Update Program.cs

--- AIOverhaulPatcher/Program.cs
+++ AIOverhaulPatcher/Program.cs
@@ -103,7 +103,7 @@ namespace AIOverhaulPatcher
                     FormKey? OverwrittingSleepingOutfit = overrides.Select(x => x.SleepingOutfit).Select(x => x.FormKey).Where(x => !x.IsNull && !OverwrittenSleepingOutfit.Contains(x)).Prepend(npc.SleepingOutfit.FormKey).LastOrDefault();
 
                     patchNpc.DefaultOutfit.SetTo(OverwrittingOutfit);
-                    if (OverwrittingSleepingOutfit != null && !OverwrittingSleepingOutfit.IsNull) patchNpc.SleepingOutfit.SetTo(OverwrittingSleepingOutfit);
+                    if (OverwrittingSleepingOutfit != null && !OverwrittingSleepingOutfit.Value.IsNull) patchNpc.SleepingOutfit.SetTo(OverwrittingSleepingOutfit);
 
                     patchNpc.SpectatorOverridePackageList.SetTo(npc.SpectatorOverridePackageList);
                     patchNpc.CombatOverridePackageList.SetTo(npc.CombatOverridePackageList);
