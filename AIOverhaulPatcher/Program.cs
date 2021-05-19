cc829b728b66b23a0a2ffd363ae8838d3492a366
Ahmed Jlassi <excinerus@gmail.com>
2021-05-19 17:42:21 +0100

    no longer produces identical to last override records

--- AIOverhaulPatcher/Program.cs
+++ AIOverhaulPatcher/Program.cs
@@ -18,7 +18,7 @@ namespace AIOverhaulPatcher
             return SynthesisPipeline.Instance
                 .AddPatch<ISkyrimMod, ISkyrimModGetter>(RunPatch, new PatcherPreferences()
                 {
-                    ExclusionMods = new List<ModKey>() 
+                    ExclusionMods = new List<ModKey>()
                     {
                          new ModKey(AioPatchName, ModType.Plugin),
                          new ModKey("Nemesis PCEA.esp", ModType.Plugin)
@@ -31,9 +31,9 @@ namespace AIOverhaulPatcher
         public static void RunPatch(IPatcherState<ISkyrimMod, ISkyrimModGetter> state)
         {
 
-            var AIOverhaul = state.LoadOrder.GetModByFileName( "AI Overhaul.esp");
+            var AIOverhaul = state.LoadOrder.GetModByFileName("AI Overhaul.esp");
             var USLEEP = state.LoadOrder.GetModByFileName("Unofficial Skyrim Special Edition Patch.esp");
-            if (USLEEP!=null ) System.Console.WriteLine("Unofficial Skyrim Special Edition Patch.esp");
+            if (USLEEP != null) System.Console.WriteLine("Unofficial Skyrim Special Edition Patch.esp");
             var UsleepOrder = state.LoadOrder.GetFileOrder("Unofficial Skyrim Special Edition Patch.esp");
             System.Console.WriteLine("at " + UsleepOrder);
 
@@ -58,7 +58,7 @@ namespace AIOverhaulPatcher
             var NPCMasters = MasterFiles.Select(x => x.Mod).NotNull().SelectMany(x => x.Npcs).Where(x => AIOFormIDs.Contains(x.FormKey)).ToList();
 
             var allOverrides = state.LoadOrder.PriorityOrder.Reverse().Skip(UsleepOrder + 1).Select(x => x.Mod).NotNull().SelectMany(x => x.Npcs).Where(x => AIOFormIDs.Contains(x.FormKey)).ToList();
-         
+
 
             System.Console.WriteLine(processed + "/" + total + " Npcs");
             foreach (var npc in AIOverhaul.Npcs)
@@ -76,72 +76,155 @@ namespace AIOverhaulPatcher
                 if (winningMaster == null) winningMaster = state.LoadOrder.PriorityOrder.Select(x => x.Mod).NotNull().SelectMany(x => x.Npcs).Where(x => x.FormKey == npc.FormKey).First();
                 var overrides = allOverrides.Where(x => x.FormKey == npc.FormKey).ToList();
 
+
+
+                bool change = false;
+
                 var patchNpc = state.PatchMod.Npcs.GetOrAddAsOverride(winningOverride);
-                if (winningOverride != npc)
+                if (npc.IsProtected() && !(patchNpc.IsProtected() || patchNpc.IsEssential()))
                 {
-                    if (npc.IsProtected()) patchNpc.Configuration.Flags.SetProtected(true, true);
+                    patchNpc.Configuration.Flags.SetProtected(true, true);
+                    change = true;
 
-                    foreach (var fac in npc.Factions)
-                        if (!patchNpc.Factions.Select(x => new KeyValuePair<FormKey,int> (x.Faction.FormKey , x.Rank)).Contains(new KeyValuePair<FormKey, int>(fac.Faction.FormKey, fac.Rank)))
+                    if (npc.Name?.String == "Fihada" && change)
+                    {
+                        change = true;
+                    }
+                }
+                foreach (var fac in npc.Factions)
+                    if (!patchNpc.Factions.Select(x => new KeyValuePair<FormKey, int>(x.Faction.FormKey, x.Rank)).Contains(new KeyValuePair<FormKey, int>(fac.Faction.FormKey, fac.Rank)))
+                    {
+                        patchNpc.Factions.Add(fac.DeepCopy());
+                        change = true;
+
+                        if (npc.Name?.String == "Fihada" && change)
                         {
-                            patchNpc.Factions.Add(  fac.DeepCopy());
+                            change = true;
                         }
+                    }
 
-                     
-                    var PackagesToRemove = Masters.SelectMany(x=>x.Packages).Select(x=>x.FormKey).Where(x => !npc.Packages.Select(x=>x.FormKey).Contains(x)).ToHashSet<FormKey>();
+                if (! patchNpc.Packages.All(x=> npc.Packages.Contains(x)) || !npc.Packages.All(x => patchNpc.Packages.Contains(x)))
+                {
+                    change = true;
+
+                    if (npc.Name?.String == "Fihada" && change)
+                    {
+                        change = true;
+                    }
+                    var PackagesToRemove = Masters.SelectMany(x => x.Packages).Select(x => x.FormKey).Where(x => !npc.Packages.Select(x => x.FormKey).Contains(x)).ToHashSet<FormKey>();
 
                     var PackagesToAdd = overrides.SelectMany(x => x.Packages).Select(x => x.FormKey).Where(x => !PackagesToRemove.Contains(x)).Distinct().ToList();
 
-                    patchNpc.Packages.Clear();
-                    PackagesToAdd.ForEach(x => patchNpc.Packages.Add(x));
+                    if (PackagesToAdd.Count > 0 || PackagesToRemove.Count > 0)
+                    {
+                        patchNpc.Packages.Clear();
+                        PackagesToAdd.ForEach(x => patchNpc.Packages.Add(x));
+                        
+                    }
+                }
 
 
-                    var OverwrittenOutfits = Masters.Select(x => x.DefaultOutfit).Select(x => x.FormKey ).Distinct().ToHashSet<FormKey>();
-                    var OverwrittenSleepingOutfit = Masters.Select(x => x.SleepingOutfit).Select(x => x.FormKey).Distinct().ToHashSet<FormKey>();
 
-                    FormKey? OverwrittingOutfit = overrides.Select(x => x.DefaultOutfit).Select(x => x.FormKey).Where(x => !x.IsNull && !OverwrittenOutfits.Contains(x)).Prepend(npc.DefaultOutfit.FormKey).LastOrDefault();
-                    FormKey? OverwrittingSleepingOutfit = overrides.Select(x => x.SleepingOutfit).Select(x => x.FormKey).Where(x => !x.IsNull && !OverwrittenSleepingOutfit.Contains(x)).Prepend(npc.SleepingOutfit.FormKey).LastOrDefault();
 
+                var OverwrittenOutfits = Masters.Select(x => x.DefaultOutfit).Select(x => x.FormKey).Distinct().ToHashSet<FormKey>();
+                var OverwrittenSleepingOutfit = Masters.Select(x => x.SleepingOutfit).Select(x => x.FormKey).Distinct().ToHashSet<FormKey>();
+
+                FormKey? OverwrittingOutfit = overrides.Select(x => x.DefaultOutfit).Select(x => x.FormKey).Where(x => !x.IsNull && !OverwrittenOutfits.Contains(x)).Prepend(npc.DefaultOutfit.FormKey).LastOrDefault();
+                FormKey? OverwrittingSleepingOutfit = overrides.Select(x => x.SleepingOutfit).Select(x => x.FormKey).Where(x => !x.IsNull && !OverwrittenSleepingOutfit.Contains(x)).Prepend(npc.SleepingOutfit.FormKey).LastOrDefault();
+
+                if (npc.DefaultOutfit.FormKey != patchNpc.DefaultOutfit.FormKey)
+                {
                     if (OverwrittingOutfit.HasValue && !OverwrittingOutfit.Value.IsNull)
                         patchNpc.DefaultOutfit.SetTo(OverwrittingOutfit);
                     else
                         patchNpc.DefaultOutfit.SetToNull();
 
-                    if (OverwrittingSleepingOutfit.HasValue && !OverwrittingSleepingOutfit.Value.IsNull)
+                    change = true;
+
+                    if (npc.Name?.String == "Fihada" && change)
+                    {
+                        change = true;
+                    }
+                }
+
+                if (npc.SleepingOutfit.FormKey != patchNpc.SleepingOutfit.FormKey)
+                {
+                        if (OverwrittingSleepingOutfit.HasValue && !OverwrittingSleepingOutfit.Value.IsNull)
                         patchNpc.SleepingOutfit.SetTo(OverwrittingSleepingOutfit);
                     else
                         patchNpc.SleepingOutfit.SetToNull();
+                    change = true;
 
+                    if (npc.Name?.String == "Fihada" && change)
+                    {
+                        change = true;
+                    }
+                }
+                if (npc.SpectatorOverridePackageList.FormKey != patchNpc.SpectatorOverridePackageList.FormKey)
+                {
                     if (!npc.SpectatorOverridePackageList.IsNull)
                         patchNpc.SpectatorOverridePackageList.SetTo(npc.SpectatorOverridePackageList);
                     else
                         patchNpc.SpectatorOverridePackageList.SetToNull();
+                    change = true;
 
-                    if (!npc.CombatOverridePackageList.IsNull)
+                    if (npc.Name?.String == "Fihada" && change)
+                    {
+                        change = true;
+                    }
+                }
+
+                if (npc.CombatOverridePackageList.FormKey != patchNpc.CombatOverridePackageList.FormKey)
+                {
+                        if (!npc.CombatOverridePackageList.IsNull)
                         patchNpc.CombatOverridePackageList.SetTo(npc.CombatOverridePackageList);
                     else
                         patchNpc.CombatOverridePackageList.SetToNull();
+                    change = true;
 
-                    patchNpc.AIData.Confidence = (Confidence)Math.Min((int)patchNpc.AIData.Confidence, (int)npc.AIData.Confidence);
-
-
-                    
-                    if (npc.VirtualMachineAdapter != null)
+                    if (npc.Name?.String == "Fihada" && change)
                     {
-                        List<IScriptEntryGetter> ScriptsToForward = npc.VirtualMachineAdapter.Scripts.Where(x => patchNpc.VirtualMachineAdapter == null || !patchNpc.VirtualMachineAdapter.Scripts.Select(x => x.Name).Contains(x.Name)).ToList();
-                        if (ScriptsToForward.Count > 0)
-                        {
-                            if (patchNpc.VirtualMachineAdapter == null)
-                                patchNpc.VirtualMachineAdapter = npc.VirtualMachineAdapter.DeepCopy();
-                            else
-                            {
-                                ScriptsToForward.ForEach(x => patchNpc.VirtualMachineAdapter.Scripts.Add(x.DeepCopy()));
-                            }
-                        }
+                        change = true;
+                    }
+                }
 
+                if (npc.AIData.Confidence != patchNpc.AIData.Confidence)
+                {
+                    patchNpc.AIData.Confidence = (Confidence)Math.Min((int)patchNpc.AIData.Confidence, (int)npc.AIData.Confidence);
+                    change = true;
+
+                    if (npc.Name?.String == "Fihada" && change)
+                    {
+                        change = true;
+                    }
+                }
+
+
+                if (npc.VirtualMachineAdapter != null)
+                {
+                    List<IScriptEntryGetter> ScriptsToForward = npc.VirtualMachineAdapter.Scripts.Where(x => patchNpc.VirtualMachineAdapter == null || !patchNpc.VirtualMachineAdapter.Scripts.Select(x => x.Name).Contains(x.Name)).ToList();
+                    if (ScriptsToForward.Count > 0)
+                    {
+                        change = true;
+
+                        if (npc.Name?.String == "Fihada" && change)
+                        {
+                            change = true;
+                        }
+                        if (patchNpc.VirtualMachineAdapter == null)
+                            patchNpc.VirtualMachineAdapter = npc.VirtualMachineAdapter.DeepCopy();
+                        else
+                        {
+                            ScriptsToForward.ForEach(x => patchNpc.VirtualMachineAdapter.Scripts.Add(x.DeepCopy()));
+                        }
                     }
 
+                }
 
+
+                if (!change)
+                {
+                    state.PatchMod.Npcs.Remove(npc); 
                 }
                 b++;
                 processed++;
