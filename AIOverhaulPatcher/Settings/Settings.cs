1253392bf9c46a2f1adb436079135347013cdb84
Ahmed Jlassi <excinerus@gmail.com>
2021-06-15 06:39:11 +0100

    Reverted ignoring Identical to last override behaviour by default. that now can be enabled in the settings. Added the following settings :
    -Ignore Identical To LastOverride
     Default = false
     When enabled the patcher will not override NPCs that are already patched
    -IgnorePlayerRecord
     Default = true
     When enabled the patcher will ignore the player record (00000007)
    -MaintainHighestProtectionLevel
     Default = true
     When enabled NPCs that are set to Essential or Protected by other mods will maintain the highest protection level

---
+++ AIOverhaulPatcher/Settings/Settings.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AIOverhaulPatcher.Settings
+{
+    class Settings
+    {
+        public bool IgnoreIdenticalToLastOverride { get; set; } = false;
+        public bool IgnorePlayerRecord { get; set; } = true;
+        public bool MaintainHighestProtectionLevel { get; set; } = true;
+    }
+}
